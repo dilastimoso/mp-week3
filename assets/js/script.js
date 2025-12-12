9d2f17988bdf0d125e2ef6e2f60792ac34e4951a
Lastimoso, Dennis Isaiah <isaiahice4@outlook.com>
2025-12-12 17:52:30 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+console.log('Before binding event');
+
+let txtInput = document.querySelector('#txtInput');
+let btnReplaceAll = document.querySelector('#btnReplaceAll');
+let txtOutput = document.querySelector('#txtOutput');
+
+btnReplaceAll.addEventListener('click',function(){
+    let str = txtInput.value;
+    let result = str.replaceAll(" ","");
+    
+    txtOutput .innerText = result;
+
+    console.log(result);
+});
